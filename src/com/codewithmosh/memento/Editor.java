4362156b23c0d219b281a482af43308975931c73
Saeed Farahani <sd.mas3@gmail.com>
2022-08-08 18:20:24 +0430

    Memento Pattern

---
+++ src/com/codewithmosh/memento/Editor.java
@@ -0,0 +1,21 @@
+package com.codewithmosh.memento;
+
+public class Editor  {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public EditorState CreateState(){
+        return new EditorState(this.content);
+    }
+
+    public void restore(EditorState editorState){
+        content = editorState.getContent();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
