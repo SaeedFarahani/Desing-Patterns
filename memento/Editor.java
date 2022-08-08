43d70578f8b80b9322c62a26e85f70634b991cf7
Saeed Farahani <sd.mas3@gmail.com>
2022-08-08 18:38:49 +0430

    Memento Pattern

---
+++ memento/Editor.java
@@ -0,0 +1,21 @@
+package memento;
+
+public class Editor  {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public EditorState CreateState(){
+        return new EditorState(this.content);
+    }
+
+    public void restore(EditorState editorState){
+        content = editorState.getContent();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
